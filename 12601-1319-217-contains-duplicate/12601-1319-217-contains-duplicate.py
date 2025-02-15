818600e755f88cf2d5efbd6bf27222d8a8fb25f6
NakShatra SharMa <77985606+nak5hatra@users.noreply.github.com>
2025-02-15 14:14:33 +0530

    Delete 12601-1319-217-contains-duplicate directory

--- 12601-1319-217-contains-duplicate/12601-1319-217-contains-duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        temp_len = len(nums)
-        my_set_len = len(set(nums))
-        if temp_len == my_set_len:
-            return False
-        else:
-            return True
-        
